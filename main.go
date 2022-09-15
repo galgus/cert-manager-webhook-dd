b4eef2d64c110b3e88511797dec0a30d2ec32dc6
Aurelien Requiem <aurelien@requiem.fr>
2022-09-15 17:20:14 +1000

    🔼 upgrade apis
    - go-ovh v1.3.0
    - k8s.io v0.24.2
    - cert-manager v1.9.1

--- main.go
+++ main.go
@@ -15,9 +15,9 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
-	"github.com/jetstack/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
-	"github.com/jetstack/cert-manager/pkg/acme/webhook/cmd"
-	"github.com/jetstack/cert-manager/pkg/issuer/acme/dns/util"
+	"github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
+	"github.com/cert-manager/cert-manager/pkg/acme/webhook/cmd"
+	"github.com/cert-manager/cert-manager/pkg/issuer/acme/dns/util"
 	"github.com/ovh/go-ovh/ovh"
 )
 
